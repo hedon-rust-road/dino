2173ec165c371b3b9d15752a1879ec015844db69
hedon954 <171725713@qq.com>
2024-08-22 00:03:21 +0800

    feat(dino): finish build command for dino

--- dino/src/cli/build.rs
+++ dino/src/cli/build.rs
@@ -1,12 +1,21 @@
+use std::env;
+
 use clap::Parser;
 
-use crate::CmdExecutor;
+use crate::{build_project, CmdExecutor};
 
 #[derive(Debug, Parser)]
 pub struct BuildOpts {}
 
 impl CmdExecutor for BuildOpts {
     async fn execute(self) -> anyhow::Result<()> {
-        todo!()
+        let cur_dir = env::current_dir()?.display().to_string();
+        let (filename, cached) = build_project(&cur_dir)?;
+        if cached {
+            eprintln!("Build success: {} (cached)", filename);
+        } else {
+            eprintln!("Build success: {}", filename);
+        }
+        Ok(())
     }
 }
